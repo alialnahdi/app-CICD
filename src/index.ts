5f3c07f29c4cd0dc810ea6bc8e1c2f32b7ec2107
Ali Sheikh Alnahdi <alialnahdi444@gmail.com>
2024-12-15 19:40:52 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
